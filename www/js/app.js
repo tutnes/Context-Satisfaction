2839ee8209f7325ba48721ee162fbe1b18a436f6
Tarjei Utnes <tutnes@gmail.com>
2016-01-22 21:02:54 +0100

    modified:   config.xml 	modified:   package.json 	new file:   resources/android/icon/drawable-hdpi-icon.png 	new file:   resources/android/icon/drawable-ldpi-icon.png 	new file:   resources/android/icon/drawable-mdpi-icon.png 	new file:   resources/android/icon/drawable-xhdpi-icon.png 	new file:   resources/android/icon/drawable-xxhdpi-icon.png 	new file:   resources/android/icon/drawable-xxxhdpi-icon.png 	new file:   resources/android/splash/drawable-land-hdpi-screen.png 	new file:   resources/android/splash/drawable-land-ldpi-screen.png 	new file:   resources/android/splash/drawable-land-mdpi-screen.png 	new file:   resources/android/splash/drawable-land-xhdpi-screen.png 	new file:   resources/android/splash/drawable-land-xxhdpi-screen.png 	new file:   resources/android/splash/drawable-land-xxxhdpi-screen.png 	new file:   resources/android/splash/drawable-port-hdpi-screen.png 	new file:   resources/android/splash/drawable-port-ldpi-screen.png 	new file:   resources/android/splash/drawable-port-mdpi-screen.png 	new file:   resources/android/splash/drawable-port-xhdpi-screen.png 	new file:   resources/android/splash/drawable-port-xxhdpi-screen.png 	new file:   resources/android/splash/drawable-port-xxxhdpi-screen.png 	new file:   resources/icon.png 	new file:   resources/ios/icon/icon-40.png 	new file:   resources/ios/icon/icon-40@2x.png 	new file:   resources/ios/icon/icon-50.png 	new file:   resources/ios/icon/icon-50@2x.png 	new file:   resources/ios/icon/icon-60.png 	new file:   resources/ios/icon/icon-60@2x.png 	new file:   resources/ios/icon/icon-60@3x.png 	new file:   resources/ios/icon/icon-72.png 	new file:   resources/ios/icon/icon-72@2x.png 	new file:   resources/ios/icon/icon-76.png 	new file:   resources/ios/icon/icon-76@2x.png 	new file:   resources/ios/icon/icon-small.png 	new file:   resources/ios/icon/icon-small@2x.png 	new file:   resources/ios/icon/icon-small@3x.png 	new file:   resources/ios/icon/icon.png 	new file:   resources/ios/icon/icon@2x.png 	new file:   resources/ios/splash/Default-568h@2x~iphone.png 	new file:   resources/ios/splash/Default-667h.png 	new file:   resources/ios/splash/Default-736h.png 	new file:   resources/ios/splash/Default-Landscape-736h.png 	new file:   resources/ios/splash/Default-Landscape@2x~ipad.png 	new file:   resources/ios/splash/Default-Landscape~ipad.png 	new file:   resources/ios/splash/Default-Portrait@2x~ipad.png 	new file:   resources/ios/splash/Default-Portrait~ipad.png 	new file:   resources/ios/splash/Default@2x~iphone.png 	new file:   resources/ios/splash/Default~iphone.png 	new file:   resources/splash.png 	modified:   www/js/app.js 	modified:   www/js/controllers.js 	new file:   www/templates/browse.html 	new file:   www/templates/login.html 	new file:   www/templates/menu.html 	new file:   www/templates/playlist.html 	new file:   www/templates/playlists.html 	new file:   www/templates/search.html

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,54 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
+
